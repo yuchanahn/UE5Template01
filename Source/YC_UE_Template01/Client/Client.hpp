dc7dfb1d4fa7296144eb55b28e0d175871be33fd
yuchanahn <ultrauc123@naver.com>
2023-05-06 23:09:12 +0900

    ㅇㅇ

---
+++ Source/YC_UE_Template01/Client/Client.hpp
@@ -0,0 +1,138 @@
+﻿#pragma once
+
+#include <YC/Coroutine/Coro.hpp>
+#include <YC/ErrorOr/ErrorOr.hpp>
+
+#include "YC_UE_Template01/GameCharacter/CharacterSystem.hpp"
+#include "YC_UE_Template01/Input/Input.hpp"
+#include "YC_UE_Template01/NetPacket/PacketAction.hpp"
+#include "YC_UE_Template01/NetPacket/PacketTypes.h"
+#include "YC_UE_Template01/Static/ResourceMap.hpp"
+#include "YC_UE_Template01/Static/Static.hpp"
+#include "YC_UE_Template01/Utils/Functional.hpp"
+#include "YC_UE_Template01/Utils/String.hpp"
+
+namespace YC { namespace Client {
+
+static void SelectCharacter(const int8 CharacterID);
+
+inline std::vector<Chanel_Player> PacOf_Player = {};
+inline ErrorOr<ANetPC*> NetPC { "Not Setting" };
+inline std::unordered_map<int32, ACharacter*> AChrPtr_List = {};
+inline TMap<FString, std::function<void()>> UI_Call;
+
+
+static void Load(const UObject* G) {
+	Coro::CoStart([](const UObject* G) -> coroutine {
+		NetPC = GetWorld(G) | GetNetPC;
+		while(!NetPC.IsOk()) {
+			co_yield wait_time { 1 };
+			NetPC = GetWorld(G) | GetNetPC;
+		}
+		FPac_Test::Bind([](FPac_Test Pac){ PacOf_Player.push_back(Pac); });
+
+		Send(FPac_Test { 1, 2, "Hello Server!" }, NetPC.Unwrap());
+	}(G));
+	UI_Call.Add(L"SpawnActor", []() {
+		Send(FPac_Test { 1, 2, "SpawnActor" }, NetPC.Unwrap());
+	});
+}
+
+inline FString GetChatStringFrom(FPac_Test Packet) {
+	//SelectCharacter(0);
+	return { Packet.ChatMassage };
+}
+
+static void PrintUELog(const FString& Msg) {
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+}
+
+static void Push_UEDisplayLog(FString Msg) {
+	//GEngine->DisplayLog (LogTemp, Warning, TEXT("Client Recv - %s"), *Msg);
+}
+static void PrintUELog_S(const std::string& Msg) {
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *FString(UTF8_TO_TCHAR(Msg.c_str())));
+}
+
+static void SendChatMessage(const FString& Msg) {
+	Send(FPac_Test { 1, 2, Msg }, NetPC.Unwrap());
+	//NetPC | Curry(Send)(FPac_Test { 1, 2, Msg });
+}
+
+static void SelectCharacter(const int8 CharacterID) {
+	Send(FPac_SpawnAndPossess { CharacterID }, NetPC.Unwrap());
+	//Send();
+}
+
+static System::Character::FPlayerData Setup_CharacterInput(
+	const ANetPC* PC,
+	const System::Character::FPlayerData& MyPlayer,
+	const UClass* IMC_SC,
+	const std::unordered_map<std::string, UClass*>& IA_SC_List
+	) {
+
+	GetSubsystem(PC) | AddMappingContext(NewObject<UInputMappingContext>(MyPlayer.AChrPtr, IMC_SC), 0);
+	
+	auto Result_Player = MyPlayer;
+
+	auto Data = std::get<System::Character::FMyCharacterData>(Result_Player.CharacterData);
+	for(const auto& [fst, snd] : IA_SC_List) {
+		 Data.IA_Map[fst] = NewObject<UInputAction>(MyPlayer.AChrPtr, snd);
+	}
+	
+	Result_Player.CharacterData = Data;
+	return Result_Player;
+}
+
+
+
+
+static System::Character::FPlayerData SE_FindMyCharacter(ANetPC* PC, FPac_GetMyCharacterIndexFromServer Packet) {
+	int8 Key = Packet.CharacterIndexOfServer;
+	
+	static ACharacter* MyCharacter = nullptr; 
+	
+	Coro::CoStart([PC]() -> coroutine {
+		while(IsValid(PC)) {
+			if(IsValid(PC->GetCharacter())) {
+				co_return;
+			}
+			co_yield wait_time { 1 };
+		}
+	}());
+	return {};
+}
+
+
+/*
+static ErrorOr<void> FindEntity_Asyc(uint64_t NetEntityID, ) {
+	
+}
+*/
+
+
+static void Loop() {
+	static ErrorOr<System::Character::FPlayerData> MyChr = Err{ std::string { "My Character Not Setting" }  };
+	static auto Characters = std::vector<System::Character::FPlayerData> {}; 
+	
+	for(auto& Pac : PacOf_Player) {
+		Pac | GetChatStringFrom | AppendFront_FStr(L"서버 메시지 : ") | PrintUELog;
+		//Pac | FindMyCharacter | 
+	}
+	
+	if(MyChr.IsOk()) {
+		const auto Input = NetPC | GetSubsystem
+								 | GetPlayerInput;
+		
+		Input | WhenErr | PrintUELog_S;
+		//Input | std::bind_front(InputCheck_, MyChr.Unwrap().CharacterData) | WhenErr | PrintUELog_S;
+	}
+	
+	PacOf_Player.clear();
+}
+
+static void Unload() {
+	
+}
+
+}}
---
+++ Source/YC_UE_Template01/Utils/Functional.hpp
@@ -0,0 +1,172 @@
+﻿#pragma once
+
+#include <concepts>
+#include <variant>
+#include <YC/ErrorOr/ErrorOr.hpp>
+
+template <typename T>
+struct function_traits;
+
+template <typename ReturnType, typename... Args>
+struct function_traits<ReturnType(Args...)>
+{
+	using return_type = ReturnType;
+	using arguments = std::tuple<Args...>;
+
+	static constexpr std::size_t arity = sizeof...(Args);
+
+	template <std::size_t N>
+	struct argument
+	{
+		static_assert(N < arity, "Error: Invalid parameter index.");
+		using type = std::tuple_element_t<N, arguments>;
+	};
+};
+
+template <typename T>
+struct function_traits : public function_traits<decltype(&T::operator())>
+{};
+
+template <typename T>
+using function_traits_t = typename function_traits<T>::arguments;
+
+template <typename T>
+struct get_fn_args_type;
+
+template <typename ReturnType, typename... Args>
+struct get_fn_args_type<ReturnType(Args...)> {
+	using type = std::tuple_element_t<0, std::tuple<Args...>>;
+};
+
+template <typename T>
+struct get_fn_args_type : public get_fn_args_type<decltype(&T::operator())> {};
+
+template <typename T>
+using get_fn_args_type_t = typename get_fn_args_type<T>::type;
+
+
+template <typename T>
+struct is_variant : std::false_type {};
+
+template <typename... Ts>
+struct is_variant<std::variant<Ts...>> : std::true_type {};
+
+template <typename T>
+concept Variant = is_variant<T>::value;
+
+template <Variant VarType, typename FnType>
+requires is_t_err_opt<std::invoke_result_t<FnType, get_fn_args_type_t<FnType>>>
+static auto operator| (const VarType& Var, const FnType& Fn) -> decltype(Fn({})) {
+	using FnArgsType = get_fn_args_type_t<FnType>;
+	return std::visit([&Fn]<typename T0>(T0&& Args) -> decltype(Fn({})) {
+			if constexpr (std::is_same_v<std::decay_t<T0>, FnArgsType>) return Fn(Args);
+			else return Err { std::string("Not Matched Variant!") };
+	}, Var);
+}
+
+template <Variant VarType, typename FnType>
+requires !is_t_err_opt<std::invoke_result_t<FnType, get_fn_args_type_t<FnType>>>
+static auto operator| (const VarType& Var, const FnType& Fn) -> ErrorOr<decltype(Fn({})), std::string> {
+	using FnArgsType = get_fn_args_type_t<FnType>;
+	return std::visit([&Fn]<typename T0>(T0&& Args) -> ErrorOr<decltype(Fn({})), std::string> {
+			if constexpr (std::is_same_v<std::decay_t<T0>, FnArgsType>) return Fn(Args);
+			else return Err { std::string("Not Matched Variant!") };
+	}, Var);
+}
+
+namespace YC_Impl {
+
+template <typename ErrorType>
+struct fn_or_type_get_error_t {
+	ErrorType Error;
+	bool IsError = true;
+};
+
+template <typename ValueType>
+struct fn_or_type_get_value_t {
+	ValueType Value;
+	bool IsOk = true;
+};
+
+}
+
+template <typename FnType, typename ValueType, typename ErrorOrType>
+concept is_function_return_type_fn_or_type_get_value_t = requires(FnType Fn, ErrorOrType Value) {
+	{ Fn(Value) } -> std::same_as<YC_Impl::fn_or_type_get_value_t<ValueType>>;
+};
+template <typename FnType, typename ErrorType, typename ErrorOrType>
+concept is_function_return_type_fn_or_type_get_error_t = requires(FnType Fn, ErrorOrType Value) {
+	{ Fn(Value) } -> std::same_as<YC_Impl::fn_or_type_get_error_t<ErrorType>>;
+};
+
+template <typename Type, typename ErrorType, typename F>
+requires is_function_return_type_fn_or_type_get_value_t<F, Type, ErrorOr<Type, ErrorType>>
+auto operator| (const ErrorOr<Type, ErrorType>& Left, F&& Fn) -> decltype(Fn(Left)) {
+	return Fn(Left);
+}
+
+template <typename Type, typename ErrorType, typename F>
+requires is_function_return_type_fn_or_type_get_error_t<F, ErrorType, ErrorOr<Type, ErrorType>>
+auto operator| (const ErrorOr<Type, ErrorType>& Left, F&& Fn) -> decltype(Fn(Left)) {
+	return Fn(Left);
+}
+
+template <typename ErrorType, typename F>
+requires is_return_type_void<F, ErrorType>
+auto operator| (YC_Impl::fn_or_type_get_error_t<ErrorType> Left, F&& Fn) -> void {
+	if(Left.IsError) {
+		Fn(Left.Error);
+	}
+}
+
+template <typename ErrorType, typename F>
+requires is_return_type_void<F>
+auto operator| (YC_Impl::fn_or_type_get_error_t<ErrorType> Left, F&& Fn) -> void {
+	if(Left.IsError) {
+		Fn();
+	}
+}
+
+
+template <typename ErrorType, typename F>
+requires !is_return_type_void<F, ErrorType>
+auto operator| (YC_Impl::fn_or_type_get_error_t<ErrorType> Left, F&& Fn) -> decltype(Fn(Left.Error)) {
+	if(Left.IsError) {
+		return Fn(Left.Error);
+	}
+	return {};
+}
+
+template <typename Type, typename F>
+requires is_return_type_void<F>
+auto operator| (YC_Impl::fn_or_type_get_value_t<Type> Left, F&& Fn) -> void {
+	if(Left.IsOk) {
+		Fn();
+	}
+}
+
+template <typename Type, typename F>
+requires is_return_type_void<F, Type>
+auto operator| (YC_Impl::fn_or_type_get_value_t<Type> Left, F&& Fn) -> void {
+	if(Left.IsOk) {
+		Fn(Left.Value);
+	}
+}
+
+template <typename Type, typename F>
+requires !is_return_type_void<F, Type>
+auto operator| (YC_Impl::fn_or_type_get_value_t<Type> Left, F&& Fn) -> decltype(Fn(Left.Value)) {
+	if(Left.IsOk) {
+		return Fn(Left.Value);
+	}
+	return {};
+}
+
+inline auto WhenOk = [](is_t_err_opt auto Left) -> YC_Impl::fn_or_type_get_value_t<decltype(Left.Unwrap())> {
+	if(Left.IsOk()) return { Left.Unwrap() };
+	return { {}, false };
+};
+inline auto WhenErr = [](is_t_err_opt auto Left) -> YC_Impl::fn_or_type_get_error_t<decltype(Left.GetError())> {
+	if(Left.IsErr()) return { Left.GetError() };
+	return { {}, false };
+};
